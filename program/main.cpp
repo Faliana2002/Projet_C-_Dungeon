b2800d23e56473375e7dd9565ac4109d01137939
cleman <cleleb179@gmail.com>
2024-04-30 17:02:30 +0200

    fichier declaration.hpp

--- program/main.cpp
+++ program/main.cpp
@@ -2,41 +2,17 @@
 #include <iostream>
 
 #include "../class/Personnages/Joueurs.hpp"
+#include "../class/declaration.hpp"
 
 using namespace sf;
 using namespace std;
 
-std::string reference  = "../0x72_DungeonTilesetII_v1.7/0x72_DungeonTilesetII_v1.7/frames/";
-std::string idle = "_idle_anim_f";
-std::string run = "_run_anim_f";
-std::string fin_str = ".png";
+extern std::string reference;
+extern std::string idle;
+extern std::string run;
+extern std::string fin_str;
 
-std::string listeJoueurs[] = {"angel",
-                            "big_demon",
-                            "big_zombie",
-                            "chort",
-                            "doc",
-                            "dwarf_f",
-                            "dwarf_m",
-                            "elf_f",
-                            "elf_m",
-                            "goblin",
-                            "imp",
-                            "knight_f",
-                            "knight_m",
-                            "lizard_f",
-                            "lizard_m",
-                            "masked_orc",
-                            "ogre",
-                            "orc_shaman",
-                            "orc_warrior",
-                            "pumpkin_dude",
-                            "skelet",
-                            "tiny_zombie",
-                            "wizzard_f",
-                            "wizzard_m",
-                            "wogol"};       //25
-// Par 4 (au lieu de 8) : zombie/slug/tiny_slug/ice_zombie/muddy/necromancer/swampy/
+extern std::string listeJoueurs[];
 
 int main() {
     RenderWindow window(VideoMode(200,200), "Rungeon!");
