e0e5c5bfe533f0bee07a9e1ffc6916d1a3ef4852
antoine <antoine.sauvenay@etu.sorbonne-universite.fr>
2024-05-20 19:28:03 +0200

    salles_test_v2.10(rebasage_manuel)

--- class/Salles/main.cpp
+++ class/Salles/main.cpp
@@ -1,9 +1,9 @@
 #include "Sallesv2.hpp"
 
 int main(){
-<<<<<<< HEAD
-Point P(5,-5);
-	Salles s(P,10,6);
+//<<<<<<< HEAD
+//Point P(5,-5);
+//	Salles s(P,10,6);
 	//s.init_texture();
 =======
     Point P(1,-1);
@@ -11,6 +11,6 @@ Point P(5,-5);
     Point fin(4,0);
 	Salles s(P,20,10,deb,fin);
 	s.init_texture();
->>>>>>> ee4ec1a (salles_test_v2.10)
+//>>>>>>> ee4ec1a (salles_test_v2.10)
 	return (0);
 }
